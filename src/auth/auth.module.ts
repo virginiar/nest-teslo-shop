a5e8371f7e80030ef76416126753c37a0940e54a
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-04-12 17:56:36 +0200

    Implementando JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,28 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
     ConfigModule,
+
     TypeOrmModule.forFeature([User]),
+
     PassportModule.register({ defaultStrategy: 'jwt' }),
+
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
-        // console.log('JWT SECRET', process.env.JWT_SECRET);
-        // console.log('JWT Secret', configService.get('JWT_SECRET'));
+        // console.log('JWT Secret', configService.get('JWT_SECRET') )
+        // console.log('JWT SECRET', process.env.JWT_SECRET)
         return {
-          secret: configService.get('JWT_SECRET'),
+          secret: configService.get<string>('JWT_SECRET'),
           signOptions: {
             expiresIn: '2h',
           },
@@ -30,11 +34,12 @@ import { User } from './entities/user.entity';
       },
     }),
     // JwtModule.register({
-    //   secret: process.env.JWT_SECRET,
-    //   signOptions: {
-    //     expiresIn: '2h',
-    //   },
-    // }),
+    // secret: process.env.JWT_SECRET,
+    // signOptions: {
+    //   expiresIn:'2h'
+    // }
+    // })
   ],
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule],
 })
 export class AuthModule {}
---
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -0,0 +1,5 @@
+export interface JwtPayload {
+  email: string;
+
+  // TODO: añadir todo lo que se quiera grabar en el payload
+}
