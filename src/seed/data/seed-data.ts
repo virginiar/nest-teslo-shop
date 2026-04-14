2193da443c890b0b8e3b8e1bccfe89c584e977cd
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-04-14 16:51:03 +0200

    Añadiendo usuarios al seed

--- src/seed/data/seed-data.ts
+++ src/seed/data/seed-data.ts
@@ -14,11 +14,34 @@ interface SeedProduct {
 type ValidSizes = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
 type ValidTypes = 'shirts' | 'pants' | 'hoodies' | 'hats';
 
+interface SeedUser {
+  email: string;
+  fullName: string;
+  password: string;
+  roles: string[];
+}
+
 interface SeedData {
+  users: SeedUser[];
   products: SeedProduct[];
 }
 
 export const initialData: SeedData = {
+  users: [
+    {
+      email: 'test1@google.com',
+      fullName: 'Test One',
+      password: 'Abc123',
+      roles: ['admin'],
+    },
+    {
+      email: 'test2@google.com',
+      fullName: 'Test Two',
+      password: 'Abc123',
+      roles: ['user', 'super'],
+    },
+  ],
+
   products: [
     {
       description:
